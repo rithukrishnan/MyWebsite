5868f0e9b0cd6ab49ea06270f37b1e5a134cedd0
Rithu Anand Krishnan <ri@rithus-air.lan>
2022-08-18 14:29:49 -0500

    Pushing Kami's code as base

---
+++ src/components/Contact.js
@@ -0,0 +1,134 @@
+import React, { useState } from "react";
+
+const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  function encode(data) {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + " = " + encodeURIComponent(data[key])
+      )
+      .join("&");
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", name, email, message }),
+    })
+      .then(() => alert("Message 📩 sent!"))
+      .catch((error) => alert("Something seems to have gone wrong!"));
+  }
+
+  return (
+    <section id="contact" className="relative">
+      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+        <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+          <iframe
+            width="100%"
+            height="100%"
+            title="map"
+            className="absolute inset-0"
+            frameBorder={0}
+            marginHeight={0}
+            marginWidth={0}
+            style={{ filter: "opacity(0.7)" }}
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6890.569276461592!2d-97.73605997287056!3d30.285957477180265!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8644b59b2584cfb7%3A0x8131ee4f174a21de!2sThe%20University%20of%20Texas%20at%20Austin!5e0!3m2!1sen!2sin!4v1654810309529!5m2!1sen!2sin"
+          />
+
+          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+            <div className="lg:w-1/2 px-6">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                Address
+              </h2>
+              <p className="mt-1">
+                University of Texax, <br />
+                Austin, TX 78712, <br /> United States
+              </p>
+            </div>
+            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                Email
+              </h2>
+              <a className="text-indigo-400 leading-relaxed">
+                rithuanandakrishnan
+                <br />
+                @utexas.edu
+              </a>
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+                Phone
+              </h2>
+              <p className="leading-relaxed">123-456-8791</p>
+            </div>
+          </div>
+        </div>
+        <form
+          netlify
+          name="contact"
+          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
+          onSubmit={handleSubmit}
+        >
+          <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+            Contact Me
+          </h2>
+          <p className="leading-relaxed mb-5">
+            If you have a great internship/full time opportunity and I suit the
+            profile, kindly contact me using the below form!
+          </p>
+          <div className="relative mb-4">
+            <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="name"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="relative mb-4">
+            <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="yourname@email.com"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="relative mb-4">
+            <label
+              htmlFor="message"
+              className="leading-7 text-sm text-gray-400"
+            >
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setMessage(e.target.value)}
+            />
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
---
+++ src/data.js
@@ -0,0 +1,46 @@
+export const projects = [
+  {
+    title: "DBMS Scripting",
+    subtitle: "All the DDMS scripts",
+    description: "All the scripts that I wrote to query our store database",
+    image:
+      "https://bs-uploads.toptal.io/blackfish-uploads/blog/post/seo/og_image_file/og_image/15493/0712-Bad_Practices_in_Database_Design_-_Are_You_Making_These_Mistakes_Dan_Social-754bc73011e057dc76e55a44a954e0c3.png",
+    link: "https://github.com/rithukrishnan/dbms/blob/main/SQL",
+  },
+  {
+    title: "Netconf Python API",
+    subtitle: "NETCONF python library ncclient",
+    description:
+      "Python APIs to communicate with a linux box via NETCONF python library ncclient.",
+    image:
+      "https://bs-uploads.toptal.io/blackfish-uploads/components/blog_post_page/content/cover_image_file/cover_image/686219/retina_1708x683_cover-0404-python-class-attributes-an-overly-thorough-guide-Waldek_Newsletter-cc8ad72af748c8f2817b4855db358c43.png",
+    link: "https://github.com/rithukrishnan/py-api",
+  },
+];
+
+export const skills = [
+  "Python",
+  "SQL",
+  "Network Programming",
+  "Docker",
+  "JAVA",
+  "NETCONF",
+];
+
+export const testimonials = [
+  {
+    name: "Dinesh Kumar",
+    image:
+      "https://media-exp1.licdn.com/dms/image/C4D03AQEmNR-VCAvvFg/profile-displayphoto-shrink_200_200/0/1516821612102?e=2147483647&v=beta&t=H8Oi5a_rw24cQ5i499c6b1LzQ_nxwY4bUQzBGseuVi4",
+    quote:
+      "'Rithu is a determined woman who has effectively bridged the gap between her undergraduate and professionallife, producing excellent results in the process. She has strong interpersonal abilities, allowing her to collaborate with and learn from the team and experienced programmers.",
+    company: "Sr Principal Software Engineer- Infinera",
+  },
+  {
+    name: "Triveni S",
+    image: "https://uvce.ac.in/img/UVCE/Dr_ThriveniJ.jpg",
+    quote:
+      "I found Rithu to be sedulous and conscientious. Herdedication to gaining quality information in Computer Science has helped her obtain one of thereputable ranks in her class.",
+    company: "HOD Computer Science- IIT Bangalore (UVCE)",
+  },
+];
