985f9675918f22f82e4e0a8a9e04d7f8378c7ae3
Rithu Anand Krishnan <ri@rithus-air.lan>
2022-08-21 17:16:26 -0500

    Changing phone number

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -63,7 +63,7 @@ const Contact = () => {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 Phone
               </h2>
-              <p className="leading-relaxed">737-420-5116</p>
+              <p className="leading-relaxed">737-420-5117</p>
             </div>
           </div>
         </div>
